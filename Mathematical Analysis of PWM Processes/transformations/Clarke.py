466d5850850d0df9cdf9ae749b70d2a59eed08cc
yasuomaidana <yasuomaidana@gmail.com>
2023-02-20 19:10:34 +0900

    Fixing clarke function

--- Mathematical Analysis of PWM Processes/transformations/Clarke.py
+++ Mathematical Analysis of PWM Processes/transformations/Clarke.py
@@ -4,8 +4,10 @@ import numpy as np
 from numpy import linalg
 
 # Here we calculate the POWER invariant transformation
-clarke_matrix = np.array(
-    [[1, -1 / 2, -1 / 2], [0, math.sqrt(3) / 2, -math.sqrt(3) / 2], 1.0 / math.sqrt(2) * np.ones(3)])
+clarke_matrix = np.sqrt(2/3) * np.array(
+    [[1, -1 / 2, -1 / 2],
+     [0, math.sqrt(3) / 2, -math.sqrt(3) / 2],
+     1.0 / math.sqrt(2) * np.ones(3)])
 inverse_clarke_matrix = linalg.inv(clarke_matrix)
 
 
