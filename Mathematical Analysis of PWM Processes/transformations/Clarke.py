fa95ca21e6e45d27a1662fdbb48c8b44ed9e192d
yasuomaidana <yasuomaidana@gmail.com>
2023-02-20 17:38:22 +0900

    Changing from matrix to array

--- Mathematical Analysis of PWM Processes/transformations/Clarke.py
+++ Mathematical Analysis of PWM Processes/transformations/Clarke.py
@@ -4,14 +4,14 @@ import numpy as np
 from numpy import linalg
 
 # Here we calculate the POWER invariant transformation
-clarke_matrix = np.matrix(
+clarke_matrix = np.array(
     [[1, -1 / 2, -1 / 2], [0, math.sqrt(3) / 2, -math.sqrt(3) / 2], 1.0 / math.sqrt(2) * np.ones(3)])
 inverse_clarke_matrix = linalg.inv(clarke_matrix)
 
 
-def clark(input_signal: np.matrix) -> np.matrix:
-    return clarke_matrix * input_signal
+def clark(input_signal: np.array) -> np.array:
+    return np.matmul(clarke_matrix, input_signal)
 
 
-def inv_clark(input_signal: np.matrix) -> np.matrix:
+def inv_clark(input_signal: np.array) -> np.array:
     return inverse_clarke_matrix * input_signal
