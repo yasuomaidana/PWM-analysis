8404bc03304963605e469232f1ba53e537ce9cef
yasuomaidana <yasuomaidana@gmail.com>
2023-02-20 17:38:55 +0900

    Changing from matrix to array, creating park function

--- Mathematical Analysis of PWM Processes/transformations/Park.py
+++ Mathematical Analysis of PWM Processes/transformations/Park.py
@@ -1,16 +1,21 @@
-import math
-
 import numpy as np
 from numpy import linalg
 
 phi = np.pi * 2 / 3
 
 
-def park_matrix(theta: float) -> np.matrix:
-    return 2 / 3 * np.matrix([[np.sin(theta), np.sin(theta - phi), np.sin(theta + phi)],
-                              [np.cos(theta), np.cos(theta - phi), np.cos(theta + phi)],
-                              1 / 2 * np.ones(3)])
+def park_matrix(theta: float) -> np.array:
+    return np.sqrt(2 / 3) * np.array([[np.sin(theta), np.sin(theta - phi), np.sin(theta + phi)],
+                                      [np.cos(theta), np.cos(theta - phi), np.cos(theta + phi)],
+                                      np.sqrt(1 / 2) * np.ones(3)])
 
 
-def inv_park_matrix(theta: float) -> np.matrix:
+def inv_park_matrix(theta: float) -> np.array:
     return linalg.inv(park_matrix(theta))
+
+
+def park(theta: float, alpha_beta_gamma: np.array) -> np.array:
+    if not isinstance(theta, float):
+        return np.array([np.matmul(park_matrix(theta_i), alpha_beta_gamma[:, i]) for i, theta_i in enumerate(theta)]) \
+            .transpose()
+    return np.matmul(park_matrix(theta), alpha_beta_gamma)
