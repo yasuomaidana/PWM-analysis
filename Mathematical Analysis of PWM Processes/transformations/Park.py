80cc7ee74a1a9e372d05ca935bb4b0f4f49441ed
yasuomaidana <yasuomaidana@gmail.com>
2023-02-23 14:56:35 +0900

    Adding inverse park function

--- Mathematical Analysis of PWM Processes/transformations/Park.py
+++ Mathematical Analysis of PWM Processes/transformations/Park.py
@@ -19,3 +19,9 @@ def park(theta: float, alpha_beta_gamma: np.array) -> np.array:
         return np.array([np.matmul(park_matrix(theta_i), alpha_beta_gamma[:, i]) for i, theta_i in enumerate(theta)]) \
             .transpose()
     return np.matmul(park_matrix(theta), alpha_beta_gamma)
+
+
+def inv_park(theta, dq):
+    if not isinstance(theta, float):
+        return np.array([np.matmul(inv_park_matrix(theta_i), dq) for i, theta_i in enumerate(theta)])
+    return np.matmul(inv_park_matrix(theta), dq)
